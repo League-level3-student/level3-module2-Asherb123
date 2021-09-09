e4140d5f9ca4c0d24856425f68e428340793cfce
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2021-09-08 17:28:51 -0700

    Work on finding longest string

--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -1,5 +1,7 @@
 package _03_More_Algorithms;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 /*
@@ -12,6 +14,76 @@ public class Algorithms {
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+    	  for (int i = 0; i < eggs.size(); i++) {
+ 			
+         	if (eggs.get(i).contains("cracked")) {
+ 				//System.out.println(eggs.get(i));
+ 				return i;
+         	//	break;
+ 			}
+ 				
+ 			
+ 		}
+    	
+    	return -1;
     }
+
+	public static boolean isPrime(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean isSquare(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static boolean isCube(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		// TODO Auto-generated method stub
+	int pearls = 0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i)==true) {
+				pearls = pearls +1;
+				
+				//return pearls;
+			}
+		}
+		
+		
+		return pearls;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		// TODO Auto-generated method stub
+		 double largeNum = 0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i)>=largeNum) {
+				largeNum =peeps.get(i);
+				
+			}
+		}
+		
+		
+		return largeNum;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		// TODO Auto-generated method stub
+		int longString = 0;
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i)) {
+				
+			}
+			
+			
+		}
+		
+		
+		return null;
+	}
 }
